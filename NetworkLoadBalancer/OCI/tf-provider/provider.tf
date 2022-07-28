78154ae6675662723777dd031081d9901fd72eeb
ojbfive <ojbfive@gmail.com>
2022-07-28 11:06:28 -0400

    update

---
+++ NetworkLoadBalancer/OCI/tf-provider/provider.tf
@@ -0,0 +1,8 @@
+provider "oci" {
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaa7t4qscet3bg5aygjrjdxakahwguwnpxyrosib3rhza7rdrfkdxa"
+  user_ocid = "ocid1.user.oc1..aaaaaaaazxgov5lqf2yg34u3ue3q5o7i2se6q2pmoiqsdve3jiyzbo4sffka"
+  private_key_path = "/home/ziggy/.oci/oci_api_key.pem"
+  fingerprint = "86:cb:c2:78:31:26:59:a6:95:f5:83:fe:11:da:d3:bc"
+  region = "us-ashburn-1"
+}
+
